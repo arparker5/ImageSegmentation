229d0b4b39f3f24549c62cd1dea4ec44a1266110
arparker5 <arparker5@alaska.edu>
2019-04-12 12:00:14 -0800

    Convolution now works

--- imageSeg.py
+++ imageSeg.py
@@ -7,30 +7,39 @@ import sys
 
 
 def convolution(img, filter, s=1):
-    (imgx, imgy) = img.shape
-    (filtx, filty) = filter.shape
+    (imgy, imgx) = img.shape
+    (filty, filtx) = filter.shape
 
     outdim = int(imgx - filtx)+1
-    outmtx = np.zeros(outdim, outdim)
+    outmtx = np.zeros((outdim, outdim))
 
-    current_y = filty
+    current_y = 0
     out_y = 0
-    while imgy - current_y >= 0:
-        current_x = filtx
+    while current_y + filty <= imgy:
+        current_x = 0
         out_x = 0
-        while imgx - current_x >= 0:
-            cutout = img[current_y:current_y + filtx, current_x:current_x + filtx]
-            outmtx[out_y, out_x] = filter * cutout
+        while current_x + filtx <= imgx:
+            a = filter * img[current_y:current_y + filty, current_x:current_x + filtx]
+            outmtx[out_y, out_x] = a.sum()
             current_x += 1
             out_x += 1
         current_y += 1
         out_y += 1
 
+    return outmtx
+
 
 np.set_printoptions(threshold=sys.maxsize)
-img = Image.open("imgtest.jpeg").convert('L')
-img.save('greyscale.png')
-x = np.asarray(img)
+img = Image.open("pixilimg.png").convert('L')
+img.save('imggrey.png')
+imgg = np.asarray(img)
 
-print(x.shape)
+filt = Image.open("filter.png").convert('L')
+filt.save('filtergrey.png')
+filtg = np.asarray(filt)
+
+
+conimg = convolution(imgg, filtg)
+
+print(conimg)
 
