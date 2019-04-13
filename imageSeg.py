c3733e9821bfbe8e4d5f160cc60abca7cb626f7f
arparker5 <arparker5@alaska.edu>
2019-04-13 00:26:59 -0800

    Added Maxpooling for downsampling

--- imageSeg.py
+++ imageSeg.py
@@ -10,8 +10,9 @@ def convolution(img, filter, s=1):
     (imgy, imgx) = img.shape
     (filty, filtx) = filter.shape
 
-    outdim = int(imgx - filtx)+1
-    outmtx = np.zeros((outdim, outdim))
+    outdim_x = int((imgx - filtx)/s) + 1
+    outdim_y = int((imgy - filty)/s) + 1
+    outmtx = np.zeros((outdim_y, outdim_x))
 
     current_y = 0
     out_y = 0
@@ -29,6 +30,31 @@ def convolution(img, filter, s=1):
     return outmtx
 
 
+def maxpool(img, filt=2, s=2):
+    (imgy, imgx) = img.shape
+
+    outdim_x = int((imgx - filt)/s) + 1
+    outdim_y = int((imgy - filt)/s) + 1
+
+    outmtx = np.zeros((outdim_y, outdim_x))
+
+    current_y = 0
+    out_y = 0
+    while current_y + filt <= imgy:
+        current_x = 0
+        out_x = 0
+        while current_x + filt <= imgx:
+            a = img[current_y:current_y + filt, current_x:current_x + filt]
+            outmtx[out_y, out_x] = np.max(a)
+            current_x += s
+            out_x += 1
+        current_y += s
+        out_y += 1
+
+    return outmtx
+
+
+
 np.set_printoptions(threshold=sys.maxsize)
 img = Image.open("pixilimg.png").convert('L')
 img.save('imggrey.png')
@@ -41,5 +67,8 @@ filtg = np.asarray(filt)
 
 conimg = convolution(imgg, filtg)
 
-print(conimg)
+mp = maxpool(conimg)
+
+print(conimg.shape)
+print(mp.shape)
 
