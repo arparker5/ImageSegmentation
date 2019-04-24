f9d1ccb789e43d8907ecce2e450762c811363fb6
arparker5 <arparker5@alaska.edu>
2019-04-23 17:55:51 -0800

    averaging out gradients before adding to weights

--- TrainCNN.py
+++ TrainCNN.py
@@ -133,89 +133,101 @@ def run(o, d, w1, w2):
     return feedforward
 
 
-def train(o, d, w1, w2, learnfactor, label):
-    c = convolution(o, d)[0]
-    z = maxpool(c)                                    # z[0] is max pool, z[1] is location of maxes
-    i = z[0].flatten()
-    n = np.zeros((8649, 1))                              # list of input nodes after activation
+def train(o, d, w1, w2, learnfactor):  # Takes (data(o[], label), d[], w1, w2, learnfactor)
+    c = []
+    z = []
+    p = []
+    n = []
+    errsig1 = []
+    weightgrad = []
+    weightgrad2 = []
+    congradient = []
+    for g in range(len(o)):
+        c.append(convolution(o[g][0], d)[0])
+        z.append(maxpool(c[g]))                                    # z[0] is max pool, z[1] is location of maxes
+        p.append(z[g][0].flatten())
+        n.append(np.zeros((8649, 1)))
 
-    for x in range(i.size):
-        n[x] = (round(activation(i[x]), 2))
+        for x in range(p[g].size):
+            n[g][x] = (round(activation(p[g][x]), 2))           # list of input nodes after activation
 
-    # print(n, "are the inputs to the net\n")            # Prints inputs into the dense network
+        # print(n, "are the inputs to the net\n")            # Prints inputs into the dense network
 
-    feedforward = np.matmul(w1, n)                       # Feed input through first layer
-    for i in range(feedforward.size):
-        feedforward[i] = activation(feedforward[i])
+        feedforward = np.matmul(w1, n[g])                   # Feed input through first layer
+        for j in range(feedforward.size):
+            feedforward[j] = activation(feedforward[j])
 
-    actlayer1 = feedforward.copy()                       # Saves the output after activation for layer 1
+        actlayer1 = feedforward.copy()                       # Saves the output after activation for layer 1
 
-    feedforward = np.matmul(w2, feedforward)             # Feed input through second layer
-    for i in range(feedforward.size):
-        feedforward[i] = activation(feedforward[i])
+        feedforward = np.matmul(w2, feedforward)             # Feed input through second layer
+        for j in range(feedforward.size):
+            feedforward[j] = activation(feedforward[j])
 
-    deltasave = feedforward.copy()
+        saveoutput = feedforward.copy()
 
-    error = np.zeros((2, 1))
-    if label == 1:                              # label == 1: training image is a car
-        error[0] = feedforward[0] - 1
-        error[1] = feedforward[1]
-        e = (feedforward[0] - 1) ** 2 + (feedforward[1]) ** 2
-    else:                                       # Else: Training image is a negative
-        error[0] = feedforward[0]
-        error[1] = feedforward[1] - 1
-        e = (feedforward[0]) ** 2 + (feedforward[1] - 1) ** 2
+        error = np.zeros((2, 1))
+        if o[g][1] == 1:                              # label == 1: training image is a car
+            error[0] = feedforward[0] - 1
+            error[1] = feedforward[1]
+            e = (feedforward[0] - 1) ** 2 + (feedforward[1]) ** 2
+        else:                                       # Else: Training image is a negative
+            error[0] = feedforward[0]
+            error[1] = feedforward[1] - 1
+            e = (feedforward[0]) ** 2 + (feedforward[1] - 1) ** 2
 
-    print("Error =", e, " ***************************")
-    print(deltasave)
+        print("Error =", e, " ***************************")
+        print(saveoutput)
 
-    errsig2 = np.zeros((2, 1))                                   # Begin calculating new weights for w2
-    for i in range(errsig2.size):
-        errsig2[i][0] = error[i] * deriveact(deltasave[i])
+        errsig2 = np.zeros((2, 1))                                   # Begin calculating new weights for w2
+        for i in range(errsig2.size):
+            errsig2[i][0] = error[i] * deriveact(saveoutput[i])
 
-    weightgrad2 = np.matmul(errsig2, np.transpose(actlayer1))
+        weightgrad2.append(np.matmul(errsig2, np.transpose(actlayer1)))
 
-    w2 = np.subtract(w2, weightgrad2)
+        errsig1.append(np.zeros((1000, 1)))                                   # Begin calculating new weights for w1
+        for i in range(errsig1[g].size):
+            errsig1[g][i][0] = error[0] * deriveact(saveoutput[0]) + error[1] * deriveact(saveoutput[1])
 
-    errsig1 = np.zeros((1000, 1))                                # Begin calculating new weights for w1
+        weightgrad.append(np.matmul(errsig1[g], np.transpose(n[g])))
+# ///////////////////////////////////////////////////////////// stop loop here. next, calculate average
+    layer2grad = np.zeros(weightgrad2[0].shape)
+    layer1grad = np.zeros(weightgrad[0].shape)
+    for j in range(len(weightgrad2)):                    # Finds the average of the weight gradients for all test images
+        layer2grad = np.add(layer2grad, weightgrad2[j])
+        layer1grad = np.add(layer1grad, weightgrad[j])
+    layer2grad = np.divide(layer2grad, len(weightgrad2))
+    layer1grad = np.divide(layer1grad, len(weightgrad))
 
-    for i in range(errsig1.size):
-        errsig1[i][0] = error[0] * deriveact(deltasave[0]) + error[1] * deriveact(deltasave[1])
+    w2 = np.subtract(w2, layer2grad)                    # nudge the weights
+    w1 = np.subtract(w1, layer1grad)
 
-    # weightgrad = np.add(errsig1, errsig1_2)
+    # k_errsig = np.zeros(errsig1[0].shape)
+    # for j in range(len(errsig1)):                       # finds the average gradient for the kernel
+    #     k_errsig = np.add(k_errsig, errsig1[j])
+    # k_errsig = np.divide(k_errsig, len(errsig1))
 
-    weightgrad = np.matmul(errsig1, np.transpose(n))
-    # print(weightgrad)
+    for g in range(len(o)):                   # Walk through all image inputs after convolution, get avg kernel gradient
+        wt = w1.transpose()
+        wt = np.matmul(wt, errsig1[g])
 
-    w1 = np.subtract(w1, weightgrad)
+        gradientmapin = np.zeros((8649, 1))
 
-    # feedforward[0] = feedforward[0] - 1
+        for i in range(n[g].size):
+            gradientmapin[i] = wt[i] * deriveact(n[g][i])
 
-    # print(delta, "is Delta\n")
+        revmp = np.zeros(c[g].shape)
 
-    wt = w1.transpose()
-    wt = np.matmul(wt, errsig1)
+        for i in range(gradientmapin.size):
+            revmp[z[g][1][i][1]][z[g][1][i][0]] = gradientmapin[i]  # puts the gradients in place to reverse max pooling
 
-    gradientmapin = np.zeros((8649, 1))
+        congradient.append(learnfactor * np.around(convolution(o[g][0], revmp)[0], 2))
+# ////////////////////////////////////////////////////////////////// stop loop here
+    avgcong = np.zeros(congradient[0].shape)
+    for j in range(len(congradient)):                                  # calculate average grade for kernels
+        avgcong = np.add(avgcong, congradient[j])
+    avgcong = np.divide(avgcong, len(congradient))
 
-    for i in range(n.size):
-        gradientmapin[i] = wt[i] * deriveact(n[i])
-
-    # print(gradientmapin, "\n")
-
-    revmp = np.zeros(c.shape)
-
-    for i in range(gradientmapin.size):
-        revmp[z[1][i][1]][z[1][i][0]] = gradientmapin[i]        # puts the gradients in place to reverse max pooling
-
-    # print(revmp)
-
-    congradient = learnfactor * np.around(convolution(o, revmp)[0], 2)
-
-    # print(congradient)
-
-    d = np.subtract(d, congradient)                             # New Kernel
-    # print(d, "New Kernel\n")
+    d = np.subtract(d, avgcong)                             # New Kernel
 
     return d, w1, w2
 
@@ -227,8 +239,8 @@ n = []
 for i in range(100):
     img = Image.open(carpics[i]).convert('L')
     negimg = Image.open(negpics[i]).convert('L')
-    o.append(scale(np.asarray(img), -1, 1))
-    n.append(scale(np.asarray(negimg), -1, 1))
+    o.append((scale(np.asarray(img), -1, 1), 1))
+    n.append((scale(np.asarray(negimg), -1, 1), 0))
 
 stddev = 1/np.sqrt(np.prod(4900))
 d = []
@@ -236,12 +248,6 @@ for i in range(3):
     d.append(np.random.normal(loc=0, scale=stddev, size=4900))  # Initializes kernel randomly on a normal distribution
     d[i] = np.reshape(d[i], (70, 70))
 
-
-'''
-stddev = 1/np.sqrt(np.prod(4))
-d = np.random.normal(loc=0, scale=stddev, size=4)  # Initializes kernel randomly on a normal distribution
-d = np.reshape(d, (2, 2))
-d = np.around(d, 2)'''
 
 stddev = 1/np.sqrt(np.prod(2000))
 w2 = np.random.normal(loc=0, scale=stddev, size=2000)  # Initializes weights randomly on a normal distribution
@@ -261,15 +267,19 @@ learnfactor = 10
 for i in range(1):
     for k in range(2):                                              # run through the training images
         for j in range(len(d)):                                     # Train all the filters'''
+batchtest = []
+batchtest.append(o[0])
+batchtest.append(o[1])
 
+d[0], w1, w2 = train(batchtest, d[0], w1, w2, learnfactor)        # Train a neg
+d[0], w1, w2 = train(batchtest, d[0], w1, w2, learnfactor)        # Train a neg
 
-d[0], w1, w2 = train(n[0], d[0], w1, w2, learnfactor, 0)        # Train a neg
-d[0], w1, w2 = train(n[0], d[0], w1, w2, learnfactor, 0)        # Train a neg
-d[0], w1, w2 = train(n[0], d[0], w1, w2, learnfactor, 0)        # Train a neg
+#d[0], w1, w2 = train(n[0], d[0], w1, w2, learnfactor)        # Train a neg
+'''d[0], w1, w2 = train(n[0], d[0], w1, w2, learnfactor, 0)        # Train a neg
 
 d[0], w1, w2 = train(o[0], d[0], w1, w2, learnfactor, 1)        # Train a car
 d[0], w1, w2 = train(o[0], d[0], w1, w2, learnfactor, 1)        # Train a car
-d[0], w1, w2 = train(o[0], d[0], w1, w2, learnfactor, 1)        # Train a car
+d[0], w1, w2 = train(o[0], d[0], w1, w2, learnfactor, 1)        # Train a car'''
 
 
 
